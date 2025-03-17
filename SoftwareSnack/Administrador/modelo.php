1bf3594e58671ab6cf87fde95aa6600291622f9e
Webcraft9 <camiloklk804@gmail.com>
2025-03-16 22:39:45 -0500

    Delete SoftwareSnack directory

--- SoftwareSnack/Administrador/modelo.php
+++
@@ -1,9 +0,0 @@
-<?php
-function conectar() {
-    $conn = new mysqli('localhost', 'root', '', 'QLOCUR'); 
-    if ($conn->connect_error) {
-        die("Conexión fallida: " . $conn->connect_error);
-    }
-    return $conn;
-}
-?>
